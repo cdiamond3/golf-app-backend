ba26371a3b2d0d3b18ec1bab07ae92512e11d023
Chris Diamond <diamond.chris.3@gmail.com>
2021-07-23 15:42:27 -0600

    Auth Working, but Nothing Else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
 
-  # before_action :authenticate
+  before_action :authenticate
 
   def authenticate
     auth_header = request.headers[:authorization]
